8c23a779c63adf5e77ccd3ee76ffb69f980d52c8
theertha120 <141700972+theertha120@users.noreply.github.com>
2024-07-21 20:35:26 +0300

    Update endorsementRoutes.js

--- artistically/server2/routes/endorsementRoutes.js
+++ artistically/server2/routes/endorsementRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { endorseSkill } = require('../controllers/endorsements');
+const { endorseSkill } = require('../controllers/endorsementController');
 
 router.post('/endorse', endorseSkill);
 
