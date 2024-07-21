1242efe835b40cec983cab43c724cd829e90814c
theertha120 <141700972+theertha120@users.noreply.github.com>
2024-07-21 20:36:46 +0300

    Update index.js

--- artistically/server2/index.js
+++ artistically/server2/index.js
@@ -5,7 +5,10 @@ const cors = require('cors');
 const authRoutes = require('./routes/authroutes');
 const connRoutes = require('./routes/connectionRoutes');
 const messageRoutes = require('./routes/messageRoutes');
-const pendingRequestsRoutes = require('./routes/pendingRequests'); 
+const pendingRequestsRoutes = require('./routes/pendingRequests');
+const endorsementsRouter = require('./routes/endorsementRoutes');
+const connectionFetchRouter = require('./routes/acceptRoutes');
+const applyRouter = require('./routes/applicationRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,26 +16,22 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'test',
-    database: 'artistically'
-});
-
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to database:', err);
-        throw err;
-    }
-    console.log('Connected to MySQL database');
-});
+    database: 'artistically',
+}).promise()
 
 app.use('/auth', authRoutes);
 app.use('/messages', messageRoutes);
 app.use('/connections', connRoutes);
-app.use('/pending', pendingRequestsRoutes); // Use /pending instead of /pending-requests
+app.use('/pending', pendingRequestsRoutes);
+app.use('/endorsements', endorsementsRouter);
+app.use('/fetch', connectionFetchRouter);
+app.use('/apply', applyRouter);
+
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log('Server is running on port ${PORT}');
 });
